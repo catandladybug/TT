7b9a757a7196d6886259a18875d8d7199f7b7272
catandladybug <98188215+catandladybug@users.noreply.github.com>
2022-04-18 15:54:01 -0700

    h

--- Tweeters - Copy/Assets/Scripts/PauseMenu.cs
+++ Tweeters - Copy/Assets/Scripts/PauseMenu.cs
@@ -78,7 +78,8 @@ public class PauseMenu : MonoBehaviour
     public void Restart()
     {
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
     }
 
